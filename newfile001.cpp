a6169a387fd87c5fa8e93b28957ef8575fc8886b
testUserBranch <testUserBranch@gmail.com>
2016-10-12 08:11:00 -0600

    add some comment in the .cpp file in branch01

--- newfile001.cpp
+++ newfile001.cpp
@@ -1,7 +1,7 @@
 //this is a new file after the readme
 //this is a new file first in the project.
 //add one line to test the status and
-//add something for the branch, add again
+//add something for the branch, add again, add third
 //add in the master
 #include <iostream>
 
