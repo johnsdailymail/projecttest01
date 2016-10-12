b9be805777cec75ddbd34f518f8f43ce15a7b5fb
test01 <test01@gmail.com>
2016-10-11 19:53:31 -0600

    first branch01 change

--- newfile001.cpp
+++ newfile001.cpp
@@ -1,6 +1,7 @@
 //this is a new file after the readme
 //this is a new file first in the project.
 //add one line to test the status and
+//add something for the branch
 #include <iostream>
 
 int main()
